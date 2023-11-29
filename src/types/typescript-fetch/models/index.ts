314944d455377c99d6d3173837a4de5e35b8fb96
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-11-29 13:16:47 +0900

    feat: Generate lapras-public-api types.

---
+++ src/types/typescript-fetch/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Activity';
+export * from './BlogArticle';
+export * from './Event';
+export * from './GetLaprasData200Response';
+export * from './GitHubRepository';
+export * from './GitHubRepositoryLanguagesInner';
+export * from './NoteArticle';
+export * from './QiitaArticle';
+export * from './SpeakerDeckSlide';
+export * from './TeratailReply';
+export * from './ZennArticle';
